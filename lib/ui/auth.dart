8a673dba189526c61725077213feabcb22c8d9bf
Gekitenius <gekitenius.yogi@gmail.com>
2021-12-26 21:00:25 +0900

    Tabビューの設定を追加

--- lib/ui/auth.dart
+++ lib/ui/auth.dart
@@ -7,19 +7,62 @@ import 'package:seed_app/view_model/Provider_auth.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
+class TabInfo {
+  String label;
+  Widget widget;
+  TabInfo(this.label, this.widget);
+}
+
 class AuthPage extends ConsumerWidget {
+  final List<TabInfo> _tabs = [
+    TabInfo("Sign in", _SignIn()),
+    TabInfo("Sign up", _SingUp())
+  ];
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final email = ref.watch(emailProvider);
     final password = ref.watch(passwordProvider);
 
     return Scaffold(
-        backgroundColor: Color(0xFF42A5F5),
+        backgroundColor: const Color(0xFF42A5F5),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              TabBar(
+                  isScrollable: false,
+                  tabs: _tabs
+                      .map((TabInfo tab) => Tab(text: tab.label))
+                      .toList()),
+            ],
           ),
         ));
+  }
+}
+
+class _SignIn extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [
+        Container(), // ロゴ表示領域
+        TextFormField(
+          decoration: InputDecoration(labelText: 'メールアドレス'),
+          onChanged: (String value) => ref.read(emailProvider),
+        ), // メールアドレス入力ボックス
+        SizedBox(), // スペーサー
+        TextFormField(), // パスワード入力ボックス
+        Container(), // msg表示領域s
+        Container(), // ユーザー登録ボタン
+      ],
+    );
+  }
+}
+
+class _SingUp extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column();
   }
 }
--- lib/ui/introduction.dart
+++ lib/ui/introduction.dart
@@ -1,1 +1,81 @@
 // only once appear
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class IntroductionPage extends ConsumerWidget {
+  final pageDecoration = PageDecoration(
+    titleTextStyle:
+        PageDecoration().titleTextStyle.copyWith(color: Colors.black),
+    bodyTextStyle: PageDecoration().bodyTextStyle.copyWith(color: Colors.black),
+    titlePadding: const EdgeInsets.all(10),
+  );
+
+  List<PageViewModel> getPages() {
+    return [
+      PageViewModel(
+        image: Image.network('/assets/images/demo.png'),
+        title: "tutorial page1",
+        body: "This page is tutorial No.1",
+        footer: const Text(
+          "here is footer.",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: Image.network('/assets/images/demo.png'),
+        title: "tutorial page2",
+        body: "This page is tutorial No.1",
+        footer: const Text(
+          "here is footer.",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: Image.network('/assets/images/demo.png'),
+        title: "tutorial page3",
+        body: "This page is tutorial No.1",
+        footer: const Text(
+          "here is footer.",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: pageDecoration,
+      ),
+      PageViewModel(
+        image: Image.network('/assets/images/demo.png'),
+        title: "tutorial page4",
+        body: "This page is tutorial No.1",
+        footer: const Text(
+          "here is footer.",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: pageDecoration,
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: IntroductionScreen(
+        globalBackgroundColor: Colors.white,
+        pages: getPages(),
+        done: const Text(
+          "Done",
+          style: TextStyle(color: Colors.black),
+        ),
+        onDone: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setBool('haveSeenIntro', true);
+        },
+        next: const Text(
+          "Next",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
