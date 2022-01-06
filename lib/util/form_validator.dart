681f0d30a9be5fba9842318c55fbbc5bf43e87d3
Gekitenius <gekitenius.yogi@gmail.com>
2022-01-06 12:22:31 +0900

    進捗保存

---
+++ lib/util/form_validator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+List<Widget>? Items;
+
+class FormValidator extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
+
+class ItemsWidget extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
