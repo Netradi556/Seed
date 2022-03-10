72a0dc689ed8b2c037bda4c7ee3143a24c83841c
Gekitenius <gekitenius.yogi@gmail.com>
2022-03-11 08:16:23 +0900

    userオブジェクト作成中

--- lib/view_model/user_object.dart
+++ lib/view_model/user_object.dart
@@ -1,16 +1,20 @@
-class User {
-  final String sex;
-  final int age;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  User(
-    this.sex,
-    this.age,
-  );
+class User {
+  final String handleName;
+  final String sex;
+  final DateTime birthDate;
+
+  User({
+    required this.handleName,
+    required this.sex,
+    required this.birthDate,
+  });
 
   Map<String, dynamic> toMap() {
     return {
       'sex': sex,
-      'age': age,
+      'age': birthDate,
     };
   }
 }
