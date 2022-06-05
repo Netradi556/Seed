ac45410e8b53e13e6d459ac7e73d2960526834e6
Gekitenius <gekitenius.yogi@gmail.com>
2022-06-06 06:39:12 +0900

    komitto

--- lib/ui/user_top/each_user.dart
+++ lib/ui/user_top/each_user.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class EachUser extends StatelessWidget {
+  const EachUser({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: (Container()),
+        body: Container(),
       ),
     );
   }
